d54c8ecc7a5f68607b2d9b51a5c5a27b67b2cfdc
GokilaUnitd <gokila@unitdtechnologies.com>
2023-11-25 10:36:35 +0530

    Task

--- src/components/TaskEmployeesModal.js
+++ src/components/TaskEmployeesModal.js
@@ -102,7 +102,7 @@ const column = [
             <tbody>
             {employees && employees.map((el)=>{
             return<tr >
-                <td>{el.first_name} || {el.employee_name}</td>
+                <td>{el.first_name || el.employee_name}</td>
                 <td>{el.team_title}</td> 
             </tr>
          })}
