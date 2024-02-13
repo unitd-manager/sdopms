5d62f91f8f2755141ac026390b61c15b40a29aee
Sabina <sabina@unitdtechnologie.com>
2024-02-13 17:30:22 +0530

    employee

--- src/components/dashboard/extraDashboard/EmployeeCard.js
+++ src/components/dashboard/extraDashboard/EmployeeCard.js
@@ -3,7 +3,43 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import img1 from '../../../assets/images/users/user4.jpg';
 
-const EmployeeCard = ({ image, title, empId, gender,projectDesignation }) => {
+const EmployeeCard = ({ image, title, empId, gender,projectDesignation,joinDate }) => {
+
+  const calculateTotalExperience = (dateJoined) => {
+    if (!dateJoined) {
+      return ""; 
+    }
+    
+
+    const joinDateTime = new Date(dateJoined);
+    
+    const currentDate = new Date();
+    
+    const difference = currentDate - joinDateTime;
+    
+    const totalYears = difference / (1000 * 60 * 60 * 24 * 365.25);
+    
+    const totalMonths = totalYears * 12;
+    
+    const years = Math.floor(totalYears);
+    const months = Math.floor(totalMonths % 12);
+
+    
+    let experienceString = '';
+    if (years > 0) {
+      experienceString += `${years} year${years > 1 ? 's' : ''}`;
+    }
+    if (months > 0) {
+      if (experienceString) {
+        experienceString += ' ';
+      }
+      experienceString += `${months} month${months > 1 ? 's' : ''}`;
+    }
+    return experienceString;
+  };
+
+  const totalExperience = calculateTotalExperience(joinDate);
+
   return (
     <>
       <Card>
@@ -16,6 +52,7 @@ const EmployeeCard = ({ image, title, empId, gender,projectDesignation }) => {
           <CardTitle tag="h4" className="fw-bold ml-3 mt-3 mb-0 profile_detail">
             <Link to={`/EmployeeEdit/${empId}?tab=1`}>{title}</Link>
             <CardSubtitle className="text-muted mt-2" style={{fontSize:15}}>{projectDesignation}</CardSubtitle>
+            <CardSubtitle className="text-muted mt-2" style={{fontSize:15}}>Experience: {totalExperience}</CardSubtitle>
           </CardTitle>
         </CardBody>
       </Card>
@@ -29,6 +66,7 @@ EmployeeCard.propTypes = {
   empId: PropTypes.any,
   projectDesignation: PropTypes.string,
   gender: PropTypes.string,
+  joinDate: PropTypes.string,
 };
 
 export default EmployeeCard;
