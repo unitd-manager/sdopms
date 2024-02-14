3613ed89957f48b85b0bd7c7f74eb8ae02ecc97b
GobikrishnanUnited <gobi@unitdtechnologies.com>
2024-02-14 09:15:07 +0530

    first commit

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,27 +3,27 @@ import axios from 'axios';
 
 dotenv.config();
 // // Define the base URL(s) conditionally
-// let baseURL;
+let baseURL;
 
-// if (process.env.NODE_ENV === 'production') {
-//   baseURL = process.env.PRODUCTION_URL;
-// } else {
-//   baseURL = process.env.TEST_URL;
-// }
+if (process.env.NODE_ENV === 'production') {
+  baseURL = process.env.REACT_APP_PRODUCTION_URL;
+} else {
+  baseURL = process.env.REACT_APP_TEST_URL;
+}
 
-// console.log('NODE_ENV:', process.env.NODE_ENV);
-// const api = axios.create({
-//   baseURL, // Use the baseURL variable 
-
-// });
-console.log('env variable',process.env.PRODUCTION_URL)
-console.log('env variable',process.env.TEST_URL)
+console.log('NODE_ENV:', process.env.NODE_ENV);
 const api = axios.create({
-    baseURL: 'http://43.228.126.245:5007',
-    //baseURL: 'http://localhost:5007',
+  baseURL, // Use the baseURL variable 
+
+});
+//console.log('env variable',process.env.REACT_APP_PRODUCTION_URL)
+//console.log('env variable',process.env.REACT_APP_TEST_URL)
+// const api = axios.create({
+//     baseURL: 'http://43.228.126.245:5007',
+//     //baseURL: 'http://localhost:5007',
     
     
-    });
+//     });
     
 export default api;
 
