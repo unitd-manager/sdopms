27644bcae79c2118ef63bafd0810d296fcb77a93
GobikrishnanUnited <gobi@unitdtechnologies.com>
2024-06-04 13:34:58 +0530

    first commit

--- src/constants/api.js
+++ src/constants/api.js
@@ -19,7 +19,8 @@ import axios from 'axios';
 //console.log('env variable',process.env.REACT_APP_PRODUCTION_URL)
 //console.log('env variable',process.env.REACT_APP_TEST_URL)
 const api = axios.create({
-    baseURL: 'http://43.228.126.245:5007',
+    // baseURL: 'http://43.228.126.245:5007',
+    baseURL:'https://sdopms.unitdtcehnologies.com:5008',
     //baseURL: 'http://localhost:5007',
     
     
